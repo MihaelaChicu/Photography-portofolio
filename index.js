621702576f2e95f23485b81e287fa6613188a8f6
Mihaela Chiculita <120111441+MihaelaChicu@users.noreply.github.com>
2023-08-15 15:41:06 +0300

    Social media header icons
      -Add social media icons in the header section and corresponding links.
      -Add images for gray icons and colorized icons
      -Add event listener for "mouseover" to make gray icons turn to colorized icons
      -Add event listener for "mouseout" to make colorized icons turn back to gray icons

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-// Function to check if an element is in the viewport
+document.addEventListener("DOMContentLoaded", function() {
+  const icons = document.querySelectorAll(".icon");
 
-function isElementInViewPort(element){
-    var rect = element.getBoundingClientRect(); //method returns a DOMRect object with info about size of an element and its position relative to the viewport
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+  icons.forEach(icon => {
+    const iconName = icon.id.replace("-icon", "");
+    const coloredSrc = `images/${iconName}-colorized.png`;
 
+    icon.addEventListener("mouseover", () => {
+      icon.src = coloredSrc;
+    });
 
-    // Function to load images when they enter the viewpo
-    function lazyLoad() {
-        var lazyImages = document.querySelectorAll('img[data-src]');
-        
-        lazyImages.forEach(function(img) {
-          if (isElementInViewport(img)) {
-            img.src = img.dataset.src;
-            img.removeAttribute('data-src');
-          }
-        });
-      }
-    
-      // Attach lazyLoad function to scroll and resize events
-      window.addEventListener('scroll', lazyLoad);
-      window.addEventListener('resize', lazyLoad);
-      
-      // Initial lazy load
-      document.addEventListener('DOMContentLoaded', lazyLoad);
+    icon.addEventListener("mouseout", () => {
+      icon.src = `images/${iconName}-gray.png`;
+    });
+  });
+});
